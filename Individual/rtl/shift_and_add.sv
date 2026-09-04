4b6334670914aa767f510a3a4cd9ffb1436c6a91
Rakesh Ratheeshan <rakesh.arulraj@gmail.com>
2026-09-04 14:59:08 +0530

    intitial accelerator mac units

---
+++ Individual/rtl/shift_and_add.sv
@@ -0,0 +1,11 @@
+module shift_and_add #( int PARAMETER WIDTH = 8)(
+    input logic [WIDTH-1] A,
+    input logic [WIDTH-1] B,
+    input logic [WIDTH-1] Q,
+    output logic [WIDTH-1] P,
+);
+    always_comb() begin
+        
+    end
+
+endmodule
